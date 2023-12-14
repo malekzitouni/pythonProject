b0db8d56c97c67861a1367b98af5fe55945c6a8f
malekzitouni <malek.zitouni@insat.ucar.tn>
2023-12-15 00:20:45 +0100

    m

--- test4.py
+++ test4.py
@@ -4,14 +4,15 @@ import numpy as np
 path = r'C:\Users\T.M.S\OneDrive\Bureau\OPENCV tuto\MONA_LISA.JPG'
 img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  # Read the image in grayscale mode
 _, mask = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
-kernel=np.ones((2,2),np.uint8)
+kernel=np.ones((3,3),np.uint8)
+print(kernel)
 dilatation=cv2.dilate(mask,kernel)
+erosion=cv2.erode(mask,kernel)
+title = ['Original Image', 'Binary Mask','dilatation','erosion']
+images = [img, mask,dilatation,erosion]
 
-title = ['Original Image', 'Binary Mask','dilatee']
-images = [img, mask,dilatation]
-
-for i in range(3):
-    plt.subplot(1, 3, i + 1)
+for i in range(4):
+    plt.subplot(1, 4, i + 1)
     plt.imshow(images[i], cmap='gray')  # Moved cmap='gray' to plt.imshow()
     plt.title(title[i])
     plt.xticks([]), plt.yticks([])  # Hide axis ticks
